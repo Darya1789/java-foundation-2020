5fd9eec6f144baedc48c0ac560c1d373dc282d4e
Darya <d.mostovich@mail.ru>
2020-11-09 00:24:16 +0300

    add in StartClass.java functionality

--- src/main/java/ru/itsjava/homework/oop1/StartClass.java
+++ src/main/java/ru/itsjava/homework/oop1/StartClass.java
@@ -7,5 +7,23 @@ public class StartClass {
         cow.setBreed("Серая");
         System.out.println("Корова по имени " + cow.getName() + ". Порода: " + cow.getBreed());
 
+        Man man = new Man("Миша", true);
+        man.setRace("Монголоидная");
+        System.out.println("man.getRace() = " + man.getRace());
+
+        Shop shop = new Shop("Великолукский");
+        shop.setShopType("Мясной");
+        System.out.println("shop.getShopType() = " + shop.getShopType());
+
+        House home = new House("Кирпичный", "улю Ленина, 22");
+        home.setFloorCount(3);
+        System.out.println("home.getFloorCount() = " + home.getFloorCount());
+
+        Mountain ural = new Mountain("Уральские");
+        ural.setHeigth(1900);
+        System.out.println("ural.getHeigth() = " + ural.getHeigth());
+
+
+
     }
 }
