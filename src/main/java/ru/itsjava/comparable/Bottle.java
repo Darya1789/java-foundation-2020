0a66904e39e76613b0069cfb955720a351f13a19
Darya <d.mostovich@mail.ru>
2020-11-28 02:02:41 +0300

    classwork

---
+++ src/main/java/ru/itsjava/comparable/Bottle.java
@@ -0,0 +1,35 @@
+package ru.itsjava.comparable;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Bottle implements Comparable<Bottle>, Cloneable{
+    private  final String material;
+    private final double mills;
+
+    @Override
+    public int compareTo(Bottle bottle) {
+        if (mills > bottle.mills) {
+            return 1;
+        } else if (mills == bottle.mills) {
+            return 0;
+        }
+
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Bottle{" +
+                "material='" + material + '\'' +
+                ", mills=" + mills +
+                '}';
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
